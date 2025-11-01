2be9271e5a0b59489d906e73e2874051a60836a5
Pol24hash <debelliena24@gmail.com>
2025-11-01 20:34:30 +0800

    chatbotv4

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app_with_token import process_database_queries, call_library_bot
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Or set specifically to ["http://localhost:5173"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class QueryRequest(BaseModel):
     prompt: str
